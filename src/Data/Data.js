26b5a5307c41fcc6546363b3688eb09553340014
JoaoPedroPaivaa <104532559+JoaoPedroPaivaa@users.noreply.github.com>
2022-12-13 20:05:06 -0300

    Add files via upload

---
+++ src/Data/Data.js
@@ -0,0 +1,117 @@
+// Sidebar imports
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+  UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+// Recent Card Imports
+import img1 from "../imgs/img1.png";
+import img2 from "../imgs/img2.png";
+import img3 from "../imgs/img3.png";
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Pedidos",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Clientes",
+  },
+  {
+    icon: UilPackage,
+    heading: 'Produtos'
+  },
+  {
+    icon: UilChart,
+    heading: 'Análise'
+  },
+];
+
+// Analytics Cards Data
+export const cardsData = [
+  {
+    title: "Vendas",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barValue: 70,
+    value: "25,970",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "Vendas",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  {
+    title: "Receita",
+    color: {
+      backGround: "linear-gradient(180deg, #FF919D 0%, #FC929D 100%)",
+      boxShadow: "0px 10px 20px 0px #FDC0C7",
+    },
+    barValue: 80,
+    value: "14,270",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Receita",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  {
+    title: "Despesas",
+    color: {
+      backGround:
+        "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
+      boxShadow: "0px 10px 20px 0px #F9D59B",
+    },
+    barValue: 60,
+    value: "4,270",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Despesas",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Andrew Thomas",
+    noti: "encomendou bateria Apple smart watch 2500mh.",
+    time: "25 segundos atrás",
+  },
+  {
+    img: img2,
+    name: "James Bond",
+    noti: "recebeu um gadget da Samsung para carregar a bateria.",
+    time: "30 minutos atrás",
+  },
+  {
+    img: img3,
+    name: "Iron Man",
+    noti: "encomendou relógio inteligente da Apple, bateria Samsung Gear 2500mh.",
+    time: "2 horas atrás",
+  },
+];
